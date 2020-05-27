d9adea11edb11c9a05171ee790e3dc1ef8932056
Namaca <48693912+udarkness@users.noreply.github.com>
2020-05-27 13:11:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //Npm & packages
-const firebase = require('firebase/@app')
+const firebase = require('firebase')
 const FieldValue = require('firebase-admin').firestore.FieldValue;
 const admin = require('firebase-admin')
 const fs = require('fs');
