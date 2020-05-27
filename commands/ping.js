c2ef7def00373a3fb48bb9d3a4d050556d5927a0
Namaca <48693912+udarkness@users.noreply.github.com>
2020-05-27 15:53:42 -0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -5,7 +5,7 @@ module.exports.run = async (client, message, args, ops, afk) => {
 var ping = Date.now() - message.createdTimestamp + " ms";
 
 
- message.channel.sendMessage("Seu ping é `" + `${Date.now() - message.createdTimestamp}` + " ms`");
+ message.channel.send("Seu ping é `" + `${Date.now() - message.createdTimestamp}` + " ms`");
 }
 
 module.exports.config = {
