3094593cecf013e920bf2152dd40c4000006afd7
Namaca <48693912+udarkness@users.noreply.github.com>
2020-05-27 15:54:23 -0300

    Add files via upload

---
+++ commands/setprefix.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (client, message, args, ops, afk, db, prefix) => {
+  if(!args[0])return message.channel.send(`Novo prefixo nao informado.`)
+
+
+  let newprefix = args[0];
+  
+  db.collection('Guild').doc(message.guild.id).update({
+    'prefix': `${newprefix}`
+  }).then(() => {
+    message.channel.send(`Prefixo alterado de ${prefix} para ${newprefix}`)
+  })
+} 
+
+module.exports.config = {
+	name:  "setprefix",
+	aliases: ["prefix", 'sprefix', 'prefixo', 'setarprefixo', 'prefixset']
+}
