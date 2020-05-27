5d7327232115426738fcbe6e54d87acbe5481dac
Namaca <48693912+udarkness@users.noreply.github.com>
2020-05-27 12:51:20 -0300

    Add files via upload

---
+++ commands/afk.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (client, message, args, ops, afk) => {
+  let reason = args.join(' ') ? args.join(' ') : 'To afk. 🏃 💻 ';
+    let afklist = client.afk.get(message.author.id);
+    if (!afklist) {
+        let construct = {
+            id: message.author.id,
+            reason: reason,
+            tag: message.author.tag
+        };
+       
+        client.afk.set(message.author.id, construct);
+       var embed = new Discord.MessageEmbed()
+       .setDescription(`**${message.author.tag}** está AFK:  \`\`${reason}\`\``)
+        return message.channel.send(embed)
+    }
+
+}
+
+module.exports.config = {
+	name:  "afk",
+	aliases: []
+}
