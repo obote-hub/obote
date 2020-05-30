3c1686b654dcf7c0f5fd84dd8740b5ef1e4491c1
Namaca <48693912+udarkness@users.noreply.github.com>
2020-05-30 19:08:16 -0300

    Create queue.js

---
+++ commands/queue.js
@@ -0,0 +1,25 @@
+module.exports.run = async (client, message, args, ops, afk, db, prefix) => {
+               
+ let fetched = ops.get(message.guild.id);
+
+ if(!fetched)return message.channel.send('Não tem nada tocando nesse server')
+
+ 	let queue = fetched.queue;
+    let nowplay = queue[0];
+
+    let resp = `\`\`\` ⬐ tocando agora\n1) ${nowplay.songtitle}\n⬑ tocando agora\n\n`
+
+    for(var i = 1; i < queue.length; i++){
+    	resp += `${i}) ${queue[i].songtitle}\n`;
+
+    }
+  
+
+    resp += `\n\n Esse é o final da lista de musica!\nUse ${prefix}play para ouvir mais.`
+  message.channel.send(resp)
+}
+
+module.exports.config = {
+	name:  "queue",
+	aliases: ["songlist", "songs", "musicas", "musiclist", "queuesongs"]
+}
