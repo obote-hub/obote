b088cff9fa34b630c876f3baef557cc869352f8d
Namaca <48693912+udarkness@users.noreply.github.com>
2020-05-30 23:36:34 -0300

    Update teste.js

--- commands/teste.js
+++ commands/teste.js
@@ -39,7 +39,7 @@ module.exports.run = async (client, message, args, ops, afk) => {
     
     else {
       let embed = new Discord.MessageEmbed()
-      .setThumbnail(`https://i.ytimg.com/vi/${video[0].id}/maxresdefault.jpg`)
+      .setThumbnail(`https://i.ytimg.com/vi/${video.id}/maxresdefault.jpg`)
       .addField(`Music`, `Playlist [${playlist.title}](${args[0]}) adicionada para a fila. [<@${message.author.id}>] `, false)
       message.channel.send(embed)
 }
