0f0a576451e245be76bfbafdd0c5349a6a5bbf74
Namaca <48693912+udarkness@users.noreply.github.com>
2020-05-31 03:15:04 -0300

    Create stop.js

---
+++ commands/stop.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js')
+const ytdl = require("ytdl-core");
+const YouTube = require("simple-youtube-api");
+const youtube = new YouTube(process.env.GOOGLE_API_KEY);
+module.exports.run = async (client, message, args, ops, afk, prefix) => {
+ const { channel } = message.member.voice;
+if (!channel) return message.channel.send('Me desculpa mas voce precisa estar em um canal de voz para usar esse comando!');
+        if (message.guild.me.voice.channel !== message.member.voice.channel) {
+            return message.channel.send("**Voce precisa estar no mesmo canal de voz que eu.!**");
+          }
+        const serverQueue = ops.get(message.guild.id);
+        if (!serverQueue) return message.channel.send('❌ **Nada tocando nesse server**');
+        
+        serverQueue.dispatcher.end()
+        ops.delete(dispatcher.guildID)
+        await message.react("👋")
+        return message.channel.send('👋 **Disconectado**')
+        
+	      
+}
+
+
+
+
+module.exports.config = {
+	name:  "stop",
+	aliases: ["parar"]
+}
