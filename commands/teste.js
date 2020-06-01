eb185e025d1c33a4b18d4818d002464b657b27f8
Namaca <48693912+udarkness@users.noreply.github.com>
2020-06-01 13:07:23 -0300

    Update teste.js

--- commands/teste.js
+++ commands/teste.js
@@ -1,4 +1,11 @@
+
+const Discord = require('discord.js')
+module.exports.run = async (client, message) => {
 let emote = '705555136981762130'
 message.react(emote);
+}
 
-
+module.exports.config = {
+	name:  "teste",
+	aliases: ["test", "testado"]
+}
