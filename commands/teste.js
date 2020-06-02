56c79c43f706d6cc396623b06db143f67a9ccde1
Namaca <48693912+udarkness@users.noreply.github.com>
2020-06-02 13:05:44 -0300

    Update teste.js

--- commands/teste.js
+++ commands/teste.js
@@ -1,7 +1,11 @@
-
+const spotify = require("spotify-url-info");
 const Discord = require('discord.js')
 module.exports.run = async (client, message, args, ops, afk) => {
-message.react('708885467852177468');
+
+
+	let track = await getData(args[0]);
+	console.log(track)
+	
 }
 
 module.exports.config = {
