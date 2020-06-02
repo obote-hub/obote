51cb36cb43cf5bf517dde5553b92aa364e14fe3f
Namaca <48693912+udarkness@users.noreply.github.com>
2020-06-01 23:44:32 -0300

    Update triggered.js

--- commands/triggered.js
+++ commands/triggered.js
@@ -4,10 +4,10 @@ const AmeAPI = new AmeClient(process.env.AME_KEY);
 module.exports.run = async (client, message, args, ops, afk) => {
 
 let user = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
-        let m = await message.channel.send("**Please wait...**");
+        let m = await message.channel.send("**Baixando a imagem...**");
         let buffer = await AmeAPI.generate("triggered", { url: user.user.displayAvatarURL({ format: "png", size: 512 }), sepia: "true", invert: "true" });
         let attachment = new Discord.MessageAttachment(buffer, "triggered.gif");
-        m.delete({ timeout: 5000 });
+        m.delete({ timeout: 3000 });
         message.channel.send(attachment);
 }
 
