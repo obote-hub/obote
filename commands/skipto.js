8cb2e209dc585b58f6a48dc1603afef06da68716
Namaca <48693912+udarkness@users.noreply.github.com>
2020-06-01 21:41:22 -0300

    Update skipto.js

--- commands/skipto.js
+++ commands/skipto.js
@@ -4,18 +4,24 @@ const YouTube = require("simple-youtube-api");
 const youtube = new YouTube(process.env.GOOGLE_API_KEY);
 
 module.exports.run = async (client, message, args, ops, afk, prefix) => {
- const { channel } = message.member.voice;
-if (!channel) return message.channel.send('Me desculpa mas voce precisa estar em um canal de voz para usar esse comando!');
-        if (message.guild.me.voice.channel !== message.member.voice.channel) {
-            return message.channel.send("**Voce precisa estar no mesmo canal de voz que eu.!**");
-          }
+  const { channel } = message.member.voice;
+  if (!channel) return message.channel.send('Me desculpa mas voce precisa estar em um canal de voz para usar esse comando!');
+  if (message.guild.me.voice.channel !== message.member.voice.channel) {
+    return message.channel.send("**Voce precisa estar no mesmo canal de voz que eu.!**");
+  }
         const fetched = ops.get(message.guild.id);
         if (!fetched) return message.channel.send('❌ **Nada tocando nesse server**');
         
         
-        
+        if(!args[0]){
+	return message.channel.send('**Coloque um numero valido, use ${prefix}queue para ver o numero da musica**');
+	}
+	if(isNaN(args[0])){
+	return message.channel.send('**Coloque um numero valido, use ${prefix}queue para ver o numero da musica**');
+	}
+	
         if (args[0] < 1 && args[0] >= fetched.queue.length) {
-            return message.channel.send('**Coloque um numero valido, use ${prefix}queue para ver o numero da musica**');
+        return message.channel.send('**Coloque um numero valido, use ${prefix}queue para ver o numero da musica**');
         }
         
          try {
