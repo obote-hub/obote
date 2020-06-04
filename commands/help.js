1ea41084222ac15313404e47617b1072b6c79501
Namaca <48693912+udarkness@users.noreply.github.com>
2020-06-03 22:41:13 -0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,8 +2,8 @@ const Discord = require('discord.js')
 const ms = require("pretty-ms");
 module.exports.run = async (client, message, args, ops, afk, db, prefix) => {
 	let commands = client.commands.map(c => `\`${c.config.name}\``).join(', ')
-
-	const embed = new Discord.MessageEmbed()
+        if(!args[0]){
+        const embed = new Discord.MessageEmbed()
 	.setTitle(`Meu prefixo é ${prefix}`)
 	.setColor("CYAN")
 	.setThumbnail(client.user.displayAvatarURL)
@@ -11,6 +11,21 @@ module.exports.run = async (client, message, args, ops, afk, db, prefix) => {
 	.addField(`Comandos gerais [${client.commands.size}]`, commands)
 	.setFooter(`${message.guild.me.displayName} | Total commands: ${client.commands.size}`)
 	message.channel.send(embed)
+	} else {
+		let command = client.commands.get(client.aliases.get(args[0].toLowerCase()) || args[0].toLowerCase());
+                if(!command) return message.channel.send(`**Não encontrei o comando ${args[0]}**`)
+		 command = command.config;
+                const commandembed = new Discord.MessageEmbed()
+                .setThumbnail(client.user.displayAvatarURL)
+                .setAuthor(`${message.guild.me.displayName} Help`, message.guild.iconURL)
+                .setTitle(`${prefix}${command.name}`)
+		.addField(`Nome`, `${command.name}`)
+                .addField(`Descrição`, `${command.description}`)
+		.addField(`Apelidos`, `${command.aliases}`)
+                .setFooter(`${message.guild.me.displayName} | Total commands: ${client.commands.size}`)
+               message.channel.send(commandembed)
+	}
+	
 }
 
 module.exports.config = {
