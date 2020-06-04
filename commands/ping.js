09e86c352fa6ef85981a11b936734d47063c3804
Namaca <48693912+udarkness@users.noreply.github.com>
2020-06-03 21:46:39 -0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -10,5 +10,6 @@ var ping = Date.now() - message.createdTimestamp + " ms";
 
 module.exports.config = {
 	name:  "ping",
-	aliases: ["pong", "pg"]
+	aliases: ["pong", "pg"],
+	description: `Ping do bot`
 }
