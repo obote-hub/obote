e6a30c03432a67e56190fdf5a50798ef5eeff543
Namaca <48693912+udarkness@users.noreply.github.com>
2020-06-07 01:10:08 -0300

    Update teste.js

--- commands/teste.js
+++ commands/teste.js
@@ -1,12 +1,18 @@
 const Discord = require('discord.js')
 module.exports.run = async (client, message, args, ops, afk, db, prefix, mute, tts) => {
+	/*
    var channels = db.collection("Channels").doc("Channels");
    let admin = require('firebase-admin');
   
  let arrUnion = channels.update({
   Channels: admin.firestore.FieldValue.arrayUnion('inilsen')
 });
+	*/
+	db.collection('Channels').doc('Channels').get().then((q) => {
+		console.log(q.data().Channels)
+	})
 	
+
 }
 
 module.exports.config = {
