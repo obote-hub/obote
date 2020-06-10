70cb9e80d87e45f5ad10b931e9b0b1154e76d150
Namaca <48693912+udarkness@users.noreply.github.com>
2020-06-09 21:31:34 -0300

    Update database.js

--- database.js
+++ database.js
@@ -3,6 +3,6 @@ const db = new pg.Client()
 
 db.connect()
 
-const res = await db.query('SELECT $1::text as message', ['Hello world!'])
+const res = db.query('SELECT $1::text as message', ['Hello world!'])
 console.log(res.rows[0].message) // Hello world!
 db.end()
