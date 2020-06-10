6e7a7b920f09c9953277f757c5e43c15fa52a424
Namaca <48693912+udarkness@users.noreply.github.com>
2020-06-09 21:44:49 -0300

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,10 @@
 const pg = require('pg')
 const db = new pg.Client({
-  user: "namaca",
-  host: "localhost",
-  database: "Obote Database",
-  password: "a29u2089#QWpos239@OApisPWMQasd"
+  user: "bdnohmjkaxgeia",
+  host: "ec2-18-232-143-90.compute-1.amazonaws.com",
+  database: "dcc8frntoebsto",
+  password: "d9e4d95612bddfdf01fc2c27e1848137840b22771f08cf3bb2f4cacb2cb9d4a9",
+  port: 5432
 })
 
 
