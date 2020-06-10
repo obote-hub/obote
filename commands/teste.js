343b5093fa47e28c08b07a09d8dbaee80e1c33e1
Namaca <48693912+udarkness@users.noreply.github.com>
2020-06-09 21:49:08 -0300

    Update teste.js

--- commands/teste.js
+++ commands/teste.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js')
+const fb = require('../database.js').db
 module.exports.run = async (client, message, args, ops, afk, db, prefix, mute, tts) => {
 	/*
    var channels = db.collection("Channels").doc("Channels");
@@ -7,12 +8,22 @@ module.exports.run = async (client, message, args, ops, afk, db, prefix, mute, t
  let arrUnion = channels.update({
   Channels: admin.firestore.FieldValue.arrayUnion('inilsen')
 });
-	*/
+	
 	db.collection('Channels').doc('Channels').get().then((q) => {
 		console.log(q.data().Channels)
 	})
+	*/
 	
-
+	
+	await fb.connect()
+        
+	
+	await fb.query(`CREATE TABLE evento(
+          ID serial PRIMARY KEY,
+          nome VARCHAR (50) UNIQUE NOT NULL
+      )`)
+	
+	await fb.end()
 }
 
 module.exports.config = {
