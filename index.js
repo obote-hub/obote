03dd6e0d169d54945d3937f75df5418cfa146836
Namaca <48693912+udarkness@users.noreply.github.com>
2020-06-11 20:54:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -54,6 +54,13 @@ const heroku = new pg.Client({
   port: 5432
 })
  heroku.connect()
+heruku.query(`CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);`)
 console.log('Database conectada PogChamp')
 client.on('ready', () => {
   console.log(`Loguei como ${client.user.tag}!, em ${client.guilds.cache.size} servers;
