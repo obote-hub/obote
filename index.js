a927855f6f01a1033a35666b3bdf94d3d4000b91
Namaca <48693912+udarkness@users.noreply.github.com>
2020-06-14 21:37:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const pg = new Client({
 pg.connect()
 
 pg.query("SELECT phones from contatos", (err, res) => {
-  const channels = res.rows
+  const channels = res.rows.phones
   console.log(channels)
 });
 
